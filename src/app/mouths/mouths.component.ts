435619b67197ff3fd8b58dc1659cab15f63dd2ed
Erin M Masterson <erin.margot.masterson@gmail.com>
2018-10-02 22:57:44 -0400

    got mouths to appear on face, must work on padding for mouth choice display next

--- src/app/mouths/mouths.component.ts
+++ src/app/mouths/mouths.component.ts
@@ -23,4 +23,88 @@ export class MouthsComponent {
         relativeContainer.innerHTML += document.getElementById("mouth1").innerHTML;
         this.buttonMouthStyle ();
     }
+    buttonMouth2 () {
+        this.buttonMouthRemove();
+        var relativeContainer = document.getElementById("relativeContainer");
+        relativeContainer.innerHTML += document.getElementById("mouth2").innerHTML;
+        this.buttonMouthStyle ();
+    }
+    buttonMouth3 () {
+        this.buttonMouthRemove();
+        var relativeContainer = document.getElementById("relativeContainer");
+        relativeContainer.innerHTML += document.getElementById("mouth3").innerHTML;
+        this.buttonMouthStyle ();
+    }
+    buttonMouth4 () {
+        this.buttonMouthRemove();
+        var relativeContainer = document.getElementById("relativeContainer");
+        relativeContainer.innerHTML += document.getElementById("mouth4").innerHTML;
+        this.buttonMouthStyle ();
+    }
+    buttonMouth5 () {
+        this.buttonMouthRemove();
+        var relativeContainer = document.getElementById("relativeContainer");
+        relativeContainer.innerHTML += document.getElementById("mouth5").innerHTML;
+        this.buttonMouthStyle ();
+    }
+    buttonMouth6 () {
+        this.buttonMouthRemove();
+        var relativeContainer = document.getElementById("relativeContainer");
+        relativeContainer.innerHTML += document.getElementById("mouth6").innerHTML;
+        this.buttonMouthStyle ();
+    }
+    buttonMouth7 () {
+        this.buttonMouthRemove();
+        var relativeContainer = document.getElementById("relativeContainer");
+        relativeContainer.innerHTML += document.getElementById("mouth7").innerHTML;
+        this.buttonMouthStyle ();
+    }
+    buttonMouth8 () {
+        this.buttonMouthRemove();
+        var relativeContainer = document.getElementById("relativeContainer");
+        relativeContainer.innerHTML += document.getElementById("mouth8").innerHTML;
+        this.buttonMouthStyle ();
+    }
+    buttonMouth9 () {
+        this.buttonMouthRemove();
+        var relativeContainer = document.getElementById("relativeContainer");
+        relativeContainer.innerHTML += document.getElementById("mouth9").innerHTML;
+        this.buttonMouthStyle ();
+    }
+    buttonMouth10 () {
+        this.buttonMouthRemove();
+        var relativeContainer = document.getElementById("relativeContainer");
+        relativeContainer.innerHTML += document.getElementById("mouth10").innerHTML;
+        this.buttonMouthStyle ();
+    }
+    buttonMouth11 () {
+        this.buttonMouthRemove();
+        var relativeContainer = document.getElementById("relativeContainer");
+        relativeContainer.innerHTML += document.getElementById("mouth11").innerHTML;
+        this.buttonMouthStyle ();
+    }
+    buttonMouth12 () {
+        this.buttonMouthRemove();
+        var relativeContainer = document.getElementById("relativeContainer");
+        relativeContainer.innerHTML += document.getElementById("mouth12").innerHTML;
+        this.buttonMouthStyle ();
+    }
+    buttonMouth13 () {
+        this.buttonMouthRemove();
+        var relativeContainer = document.getElementById("relativeContainer");
+        relativeContainer.innerHTML += document.getElementById("mouth13").innerHTML;
+        this.buttonMouthStyle ();
+    }
+    buttonMouth14 () {
+        this.buttonMouthRemove();
+        var relativeContainer = document.getElementById("relativeContainer");
+        relativeContainer.innerHTML += document.getElementById("mouth14").innerHTML;
+        this.buttonMouthStyle ();
+    }
+    buttonMouth15 () {
+        this.buttonMouthRemove();
+        var relativeContainer = document.getElementById("relativeContainer");
+        relativeContainer.innerHTML += document.getElementById("mouth15").innerHTML;
+        this.buttonMouthStyle ();
+    }
 }
