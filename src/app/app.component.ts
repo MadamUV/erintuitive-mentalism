1d3998334179d9821dfe414639053b15ebe78ca5
Erin M Masterson <erin.margot.masterson@gmail.com>
2018-10-06 17:58:00 -0400

    changed beard/mouth z-index to 5 and removed item names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,17 @@ import { delay } from '../../node_modules/@types/q';
 })
 export class AppComponent {
   title = 'Erintuitive\'s drawing interpretation app!';
-  item = "body";
   mainMessage : string = "I interpret your personality from your avatar, too!";
   showMouths () {
     setTimeout(function(){
       document.getElementById("mouthstuff").style.display = "block";
       this.mainMessage = "";
     }, 300);
-    this.item = "mouth";
   }
   showHairs () {
     setTimeout(function(){
       document.getElementById("hairstuff").style.display = "block";
       document.getElementById("mouthstuff").innerHTML = "";
     }, 300);
-    this.item = "hair";
   }
 }
